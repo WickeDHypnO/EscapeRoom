15787fca325b9cc6c65b32128f4ab5f797d3f4d8
Krystian Lison <krystianlison@wenklystudio.com>
2017-11-29 23:24:14 +0100

    Dodany targeter, outliny do itemów i system przenoszenia itemów na scenie

--- Assets/UsableTargeter.cs
+++ Assets/UsableTargeter.cs
@@ -2,25 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UsableTargeter : MonoBehaviour {
+public class UsableTargeter : MonoBehaviour
+{
 
     public LayerMask raycastMask;
+    public float itemPickupDistance = 2f;
     RaycastHit hitInfo;
     GameObject targetedItem;
-	
-	void Update () {
-		if(Physics.Raycast(transform.position, transform.forward, out hitInfo, 2f, raycastMask))
+    GameObject pickedUpItem;
+
+    void FixedUpdate()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, 50f, raycastMask))
         {
             if (targetedItem != hitInfo.collider.gameObject)
             {
-                targetedItem = hitInfo.collider.gameObject;               
+                if (targetedItem && targetedItem.GetComponent<HighlightItem>())
+                {
+                    targetedItem.GetComponent<HighlightItem>().OutlineOff();
+                }
+                targetedItem = hitInfo.collider.gameObject;
+
+                if (targetedItem.GetComponent<HighlightItem>() && Vector3.Distance(transform.position, targetedItem.transform.position) < itemPickupDistance)
+                {
+                    targetedItem.GetComponent<HighlightItem>().OutlineOn();
+                }
+            }
+            if(targetedItem.GetComponent<HighlightItem>() && Vector3.Distance(transform.position, targetedItem.transform.position) < itemPickupDistance)
+            {
+                if(!targetedItem.GetComponent<HighlightItem>().outlineOn)
+                {
+                    targetedItem.GetComponent<HighlightItem>().OutlineOn();
+                }
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.E) && targetedItem.GetComponent<Item>())
+        if (Input.GetKeyDown(KeyCode.E) && targetedItem.GetComponent<DraggableItem>() && Vector3.Distance(transform.position, targetedItem.transform.position) < itemPickupDistance)
+        {
+            pickedUpItem = targetedItem;
+            pickedUpItem.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            pickedUpItem.transform.SetParent(transform);
+        }
+
+        if (Input.GetKeyUp(KeyCode.E) && pickedUpItem)
+        {
+            pickedUpItem.transform.SetParent(null);
+            pickedUpItem.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            pickedUpItem = null;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && targetedItem.GetComponent<Item>() && Vector3.Distance(transform.position, targetedItem.transform.position) < itemPickupDistance)
         {
             GetComponentInParent<PlayerInventory>().AddItem(targetedItem.GetComponent<Item>());
+            targetedItem.GetComponent<Collider>().enabled = false;
+            targetedItem.GetComponent<HighlightItem>().OutlineOff();
             targetedItem = null;
         }
     }
+
 }
