c0b42a4684ff21019b698e94e4302209586c29bb
Krystian Lison <krystianlison@wenklystudio.com>
2017-12-10 19:14:06 +0100

    Pigowanie dla obydwóch graczy, lokalnie

--- Assets/PingDissapear.cs
+++ Assets/PingDissapear.cs
@@ -1,27 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PingDissapear : MonoBehaviour {
+public class PingDissapear : Photon.PunBehaviour {
 
     public List<GameObject> pingElements;
     bool dissapear;
     float timer;
     public float dissapearTime = 1f;
     public float scaleFactor = 0.25f;
+    public PingIndicator indicator;
+    public Color firstPlayerColor;
+    public Color secondPlayerColor;
+    public bool debugSecondPlayer;
 
     public void Restart()
     {
-        timer = 0;
-        foreach (GameObject go in pingElements)
-        {
-            go.GetComponent<Renderer>().sharedMaterial.SetFloat("_Alpha", 1);
-        }
-        transform.localScale = Vector3.zero;
+        photonView.RPC("RpcShowPing", PhotonTargets.All);
     }
 
     private void OnEnable()
     {
+        if(debugSecondPlayer)
+        {
+            foreach(Image i in indicator.GetComponentsInChildren<Image>(true))
+            {
+                i.color = secondPlayerColor;
+            }
+            pingElements[0].GetComponent<Renderer>().sharedMaterial.SetColor("_Color", secondPlayerColor);
+        }
+        else
+        {
+            foreach (Image i in indicator.GetComponentsInChildren<Image>(true))
+            {
+                i.color = firstPlayerColor;
+            }
+            pingElements[0].GetComponent<Renderer>().sharedMaterial.SetColor("_Color", firstPlayerColor);
+        }
         Restart();
         dissapear = true;
     }
@@ -37,9 +53,25 @@ public class PingDissapear : MonoBehaviour {
             transform.localScale = new Vector3(timer / dissapearTime * scaleFactor, timer / dissapearTime * scaleFactor, timer / dissapearTime * scaleFactor);
             if (timer/dissapearTime >= 1)
             {
+                foreach (GameObject go in pingElements)
+                {
+                    go.GetComponent<Renderer>().sharedMaterial.SetFloat("_Alpha", 0);
+                }
                 dissapear = false;
-                enabled = false;
+                gameObject.SetActive(false);
             }
         }
 	}
+
+    [PunRPC]
+    void RpcShowPing()
+    {
+        timer = 0;
+        foreach (GameObject go in pingElements)
+        {
+            go.GetComponent<Renderer>().sharedMaterial.SetFloat("_Alpha", 1);
+        }
+        transform.localScale = Vector3.zero;
+        indicator.StartShowing(dissapearTime);
+    }
 }
--- Assets/Pinger.cs
+++ Assets/Pinger.cs
@@ -6,17 +6,30 @@ public class Pinger : MonoBehaviour
 {
     private RaycastHit hit;
     public GameObject debugCube;
+    public float delay = 3.5f;
+    bool canPing;
+    float timer = 3.6f;
 
     void Update()
     {
-        if (Input.GetButtonDown("Ping"))
+        if (!canPing)
+        {
+            timer += Time.deltaTime;
+            if(timer >= delay)
+            {
+                canPing = true;
+            }
+        }
+        if (Input.GetButtonDown("Ping") && canPing)
         {
             if (Physics.Raycast(transform.position, transform.forward, out hit, 100f))
             {
+                timer = 0;
+                canPing = false;
                 debugCube.transform.parent = null;
                 debugCube.transform.position = hit.point;
                 debugCube.transform.up = hit.normal;
-                debugCube.GetComponent<PingDissapear>().enabled = true;
+                debugCube.SetActive(true);
                 debugCube.GetComponent<PingDissapear>().Restart();
                 GetComponent<LineRenderer>().SetPosition(0, new Vector3(transform.position.x, transform.position.y - 0.2f, transform.position.z));
                 GetComponent<LineRenderer>().SetPosition(1, debugCube.transform.position);
