fc23c69400896c1e2f706cc4b31867267f972b63
Krystian Lison <krystianlison@wenklystudio.com>
2017-12-25 00:06:08 +0100

    Działająca latarka

---
+++ Assets/FlashlightController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightController : Photon.PunBehaviour
+{
+
+    public bool flashlightEnabled = false;
+    bool flashlightOn = false;
+    public Light flashlight;
+
+    void Update()
+    {
+        if (flashlightEnabled)
+        {
+            if (Input.GetButtonDown("Flashlight"))
+            {
+                photonView.RPC("RpcSwitchFlashlight", PhotonTargets.All, flashlightOn = !flashlightOn);
+            }
+        }
+    }
+
+    [PunRPC]
+    void RpcSwitchFlashlight(bool switchFlashlight)
+    {
+        if (switchFlashlight)
+        {
+            flashlight.enabled = true;
+        }
+        else
+        {
+            flashlight.enabled = false;
+        }
+    }
+}
--- Assets/Item.cs
+++ Assets/Item.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Item : Photon.PunBehaviour {
     public Sprite itemImage;
+    public bool flashlight = false;
 
     public void HideItem(int viewID)
     {
@@ -13,9 +14,18 @@ public class Item : Photon.PunBehaviour {
     [PunRPC]
     void RpcHideItem(int viewID)
     {
-        GetComponent<MeshRenderer>().enabled = false;
+        if (GetComponent<MeshRenderer>())
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+        }
+        else
+        {
+            GetComponentInChildren<MeshRenderer>().enabled = false;
+        }
         GetComponent<HighlightItem>().OutlineOff();
         transform.parent = PhotonView.Find(viewID).transform;
+        if(flashlight)
+        transform.parent.GetComponentInChildren<FlashlightController>().flashlightEnabled = true;
         transform.localPosition = Vector3.zero;
     }
 }
