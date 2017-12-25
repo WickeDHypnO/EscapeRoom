f65fd0303d3c123c34fdd8bc8f111ea772d851d6
Krystian Lison <krystianlison@wenklystudio.com>
2017-12-25 22:40:11 +0100

    Nowa postać, animacje, patrzenie na mysz itp.

---
+++ Assets/NeckRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeckRotation : MonoBehaviour {
+
+    public Transform neck;
+
+    void Update()
+    {
+        if (transform.eulerAngles.x < 45 || transform.eulerAngles.x > 315)
+        {
+            neck.rotation = transform.rotation;
+        }
+    }
+}
--- Assets/Scripts/Items/FlashlightController.cs
+++ Assets/Scripts/Items/FlashlightController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class FlashlightController : Photon.PunBehaviour
 {
-
     public bool flashlightEnabled = false;
     bool flashlightOn = false;
     public Light flashlight;
+    public Animator playerAnimator;
+    public GameObject flashlightInHand;
 
     void Update()
     {
@@ -25,11 +26,15 @@ public class FlashlightController : Photon.PunBehaviour
     {
         if (switchFlashlight)
         {
+            playerAnimator.SetBool("hasFlashlight", true);
             flashlight.enabled = true;
+            flashlightInHand.SetActive(true);
         }
         else
         {
+            playerAnimator.SetBool("hasFlashlight", false);
             flashlight.enabled = false;
+            flashlightInHand.SetActive(false);
         }
     }
 }
