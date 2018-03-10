b30ed670e93950aeda3ca3b5b52687fe6b24d02d
Krystian Lison <krystianlison@wenklystudio.com>
2018-03-10 12:19:33 +0100

    czyszczenie struktury part 2

---
+++ Assets/Scripts/AnimationsController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationsController : MonoBehaviour {
+
+    public float movementSpeed;
+    public RigidbodyFirstPersonController playerRigidbody;
+    public Animator playerAnimator;
+	
+	void FixedUpdate () {
+        movementSpeed = playerRigidbody.Velocity.magnitude;
+        playerAnimator.SetFloat("movementSpeed", movementSpeed);
+	}
+}
---
+++ Assets/Scripts/NeckRotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeckRotation : MonoBehaviour
+{
+    public Animator animator;
+    public Transform lookPoint;
+    public Transform camera;
+
+    void OnAnimatorIK()
+    {
+        if (lookPoint != null)
+        {
+            animator.SetLookAtWeight(0.3f);
+            animator.SetLookAtPosition(lookPoint.position);
+        }
+    }
+    //void Update()
+    //{
+    //    if (transform.eulerAngles.x < 45 || transform.eulerAngles.x > 315)
+    //    {
+    //        neck.rotation = transform.rotation;
+    //    }
+    //}
+}
