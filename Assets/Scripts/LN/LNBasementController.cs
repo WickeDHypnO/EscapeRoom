a2a04cd9348120be4f3efa8ea3d444ba40c3d71e
LukaszIO <lukanow462@student.polsl.pl>
2018-06-08 21:08:14 +0200

    Skrypt piwnicy

---
+++ Assets/Scripts/LN/LNBasementController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LNBasementController : RoomController
+{
+    public GameObject Lever;
+    public GameObject ElevatorDoors;
+
+    public void EnableLever()
+    {
+        Lever.GetComponent<LeverController>().canUse = true;
+    }
+
+    public void UnlockElevator()
+    {
+        ElevatorDoors.GetComponent<ElevatorDoorController>().Open();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
