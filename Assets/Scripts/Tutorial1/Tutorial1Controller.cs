ee9dc64389c45e0d1636d00a8c4421ddf84336a1
Rafal Potoczek <rafal.potoczek@gmail.com>
2018-06-09 15:57:33 +0200

    Fix Photon Tutorial1 room

--- Assets/Scripts/Tutorial1/Tutorial1Controller.cs
+++ Assets/Scripts/Tutorial1/Tutorial1Controller.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Tutorial1Controller : MonoBehaviour {
+public class Tutorial1Controller : Photon.PunBehaviour, IPunObservable
+{
 
     public DoorController door;
     public GameObject blockingVolume;
     private bool leverDown = false;
     private bool[] platesPressed = { false, false };
-
     void Start()
     {
         if(!FindObjectOfType<LoadingScreenCanvas>())
@@ -20,11 +20,17 @@ public class Tutorial1Controller : MonoBehaviour {
         {
             FindObjectOfType<LoadingScreenCanvas>().FinishLoading();
         }
-        
     }
 
-	public void PlatePressed(int id)
+    [PunRPC]
+    private void RPCCloseDoor()
     {
+        if (!leverDown)
+            door.Close();
+    }
+    public void PlatePressed(int id)
+    {
+
         platesPressed[id] = true;
         if (platesPressed[0] && platesPressed[1])
         {
@@ -41,7 +47,22 @@ public class Tutorial1Controller : MonoBehaviour {
     public void PlateReleased(int id)
     {
         platesPressed[id] = false;
-        if(!leverDown)
-            door.Close();
+        photonView.RPC("RPCCloseDoor", PhotonTargets.All);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(platesPressed[0]);
+            stream.SendNext(platesPressed[1]);
+            stream.SendNext(leverDown);
+        }
+        else
+        {
+            platesPressed[0] = (bool)stream.ReceiveNext();
+            platesPressed[1] = (bool)stream.ReceiveNext();
+            leverDown = (bool)stream.ReceiveNext();
+        }
     }
 }
