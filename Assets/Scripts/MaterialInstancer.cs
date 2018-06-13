ba4592e9b8e7f4a7fec8bd4e9bafce03668ac75f
Krystian Lison <lisonkrystian@gmail.com>
2018-06-13 15:20:04 +0200

    Czyszczenie projektu, dodany skrypt zmieniający materiał na instancję materiału

---
+++ Assets/Scripts/MaterialInstancer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialInstancer : MonoBehaviour {
+
+	public Color materialColor;
+	public Material defaultMaterial;
+	
+	[ContextMenu("Set new material")]
+	void MakeMaterialInstance()
+	{
+		var newMaterial = new Material(defaultMaterial);
+		newMaterial.color = materialColor;
+		DestroyImmediate(GetComponent<MeshRenderer>().material);
+		GetComponent<MeshRenderer>().material = newMaterial;
+	}
+}
