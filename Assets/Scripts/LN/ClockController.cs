5c110bc7fa6dd234ef284cbe6dd8320347167d0b
LukaszIO <lukanow462@student.polsl.pl>
2018-06-13 14:32:58 +0200

    Poprawki do skryptu zegara, zmiany w trzecim pokoju

--- Assets/Scripts/LN/ClockController.cs
+++ Assets/Scripts/LN/ClockController.cs
@@ -14,21 +14,19 @@ public class ClockController : MonoBehaviour
     //private const float HOUR_POINTER_FULL_ROTATION_TIME = 24.0f * MINUTE_POINTER_FULL_ROTATION_TIME;
     private float elapsedTime;
     private float direction;
-    private float oneOverTimeScale;
 
 	// Use this for initialization
 	void Start ()
     {
         elapsedTime = TimeScale * 0.5f;
         direction = 1.0f;
-        oneOverTimeScale = 1.0f / TimeScale;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
         float delta = Time.deltaTime;
-        float scaledDelta = delta * oneOverTimeScale;
+        float scaledDelta = delta / TimeScale;
         float minuteZInc = scaledDelta * 360.0f / MINUTE_POINTER_FULL_ROTATION_TIME;
         float hourZInc = minuteZInc / 24.0f;//delta * 360.0f / HOUR_POINTER_FULL_ROTATION_TIME;
         float pendulumZInc = PendulumAngle * 2.0f * scaledDelta;
@@ -36,12 +34,12 @@ public class ClockController : MonoBehaviour
         Vector3 hourRotation = HourPointer.transform.localRotation.eulerAngles;
         Vector3 pendulumRotation = ClockPendulum.transform.localRotation.eulerAngles;
         float minuteZ = minuteRotation.z + minuteZInc;
-        if (minuteZ >= 360.0f)
+        if (minuteZ > 360.0f)
         {
             minuteZ = minuteZ - 360.0f;
         }
         float hourZ = hourRotation.z + hourZInc;
-        if (hourZ >= 360.0f)
+        if (hourZ > 360.0f)
         {
             hourZ = hourZ - 360.0f;
         }
@@ -52,7 +50,7 @@ public class ClockController : MonoBehaviour
             float oldDir = direction;
             direction *= (-1.0f);
             elapsedTime = elapsedTime - TimeScale;
-            pendulumZ = (oldDir * PendulumAngle) + (direction * PendulumAngle * 2.0f * elapsedTime * oneOverTimeScale);
+            pendulumZ = (oldDir * PendulumAngle) + (direction * PendulumAngle * 2.0f * elapsedTime / TimeScale);
         }
         MinutePointer.transform.localRotation = Quaternion.Euler(minuteRotation.x, minuteRotation.y, minuteZ);
         HourPointer.transform.localRotation = Quaternion.Euler(hourRotation.x, hourRotation.y, hourZ);
