77ee4851fcaa847bef08b5892ab0140476d34660
Rafal Potoczek <rafal.potoczek@gmail.com>
2018-06-17 10:31:26 +0200

    Reading items, UI

---
+++ Assets/Scripts/UI/TextViewer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextViewer : UsableTarget {
+
+    public Canvas canvas;
+    public string text;
+    private bool isReading = false;
+    private RigidbodyFirstPersonController playerRigidbody;
+    public void SetPlayerRigidbody(RigidbodyFirstPersonController _playerRigidbody)
+    {
+        playerRigidbody = _playerRigidbody;
+    }
+    public override void Use()
+    {
+        if (!isReading)
+        {
+            isReading = true;
+            playerRigidbody.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            playerRigidbody.mouseEnabled = false;
+            canvas.GetComponentInChildren<Text>().text = text;
+            canvas.gameObject.SetActive(true);
+        }
+        else
+        {
+            isReading = false;
+            playerRigidbody.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+            playerRigidbody.mouseEnabled = true;
+            canvas.gameObject.SetActive(false);
+        }
+    }
+
+    public override void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        return;
+    }
+}
