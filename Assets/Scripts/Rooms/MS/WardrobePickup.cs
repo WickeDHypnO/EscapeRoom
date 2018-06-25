99fb74026c73c8fe83b22028a89907009bca0550
Marcin <marcinsroda94@gmail.com>
2018-06-26 00:36:32 +0200

    Kółka do szafy

--- Assets/Scripts/Rooms/MS/WardrobePickup.cs
+++ Assets/Scripts/Rooms/MS/WardrobePickup.cs
@@ -7,15 +7,13 @@ public class WardrobePickup : MonoBehaviour {
     public float m_maxHeight;
     public float m_minHeight;
 
-    [SerializeField] private float m_minInteractHeight;
-    [SerializeField] private GameObject m_objectUnder;
-
+    private void Start()
+    {
+    }
 
     void Update ()
     {
         this.transform.position = new Vector3(this.transform.position.x, Mathf.Max(m_minHeight, Mathf.Min(m_maxHeight, this.transform.position.y)), this.transform.position.z);
-
-        m_objectUnder.SetActive(this.transform.position.y >= m_minInteractHeight);
 	}
 
     public void SetMinHeight(float height)
